e7ce2d60118614409df906e7f3dec8bc93866f3e
luis berrezueta <laberrezueta2@protonmail.com>
2023-09-25 09:31:20 -0500

    images added

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -44,4 +44,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Una serie de controles en flutter',
       link: '/ui-controls',
       icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subTitle: 'Pequeño tutorial introductorio',
+      link: '/tutorial',
+      icon: Icons.document_scanner_outlined),
 ];
